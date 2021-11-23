c2dca61795009a22587e0d758b7252f8eb1cbf0a
Guilherme Fernandes <57098649+guilfer-dev@users.noreply.github.com>
2021-11-23 00:20:46 -0300

    [feat] add shorcut for toogle fullscreen

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 app.on('ready', function () {
   const win = new BrowserWindow({
-    width: 1920,
-    height: 1080,
+    width: 1280,
+    height: 720,
     autoHideMenuBar: true,
     fullscreen: true,
     icon: __dirname + '/favicon.ico',
@@ -15,17 +15,15 @@ app.on('ready', function () {
     }
   });
 
-  session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    details.requestHeaders['User-Agent'] = 'Mozilla/5.0 (SMART-TV; LINUX; Tizen 5.5) AppleWebKit/537.36 (KHTML, like Gecko) 69.0.3497.106.1/5.5 TV Safari/537.36';
-    callback({ cancel: false, requestHeaders: details.requestHeaders });
-  });
+  win.webContents.on('before-input-event', (event, input) => {
+    if (input.alt && input.key === 'Enter') {
+      win.setFullScreen(!win.isFullScreen());
+      event.preventDefault();
+    }
+  })
 
-  win.on('close', (e) => {
-    e.preventDefault();
-    win.hide();
-    win.setSkipTaskbar(true);
+  win.loadURL('https://www.youtube.com/tv', {
+    userAgent: 'Mozilla/5.0 (SMART-TV; LINUX; Tizen 5.5) AppleWebKit/537.36 (KHTML, like Gecko) 69.0.3497.106.1/5.5 TV Safari/537.36'
   });
-
-  win.loadURL('https://www.youtube.com/tv');
 
 });
